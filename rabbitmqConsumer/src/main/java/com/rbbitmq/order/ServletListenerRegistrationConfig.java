fa6349d5281667a39f0598bb293c1263f8210459
dongchenhao <605020865@qq.com>
2021-04-02 11:02:25 +0800

    Update ServletListenerRegistrationConfig.java

--- rabbitmqConsumer/src/main/java/com/rbbitmq/order/ServletListenerRegistrationConfig.java
+++ rabbitmqConsumer/src/main/java/com/rbbitmq/order/ServletListenerRegistrationConfig.java
@@ -4,18 +4,7 @@ import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Copyright © 2018 五月工作室. All rights reserved.
- *
- * @Package com.amos.common.config
- * @ClassName ServletListenerRegistrationConfig
- * @Description 在容器启动的时候，注册自定义的Listener
- * 1. 在监听器中初始化线程池
- * @Author Amos
- * @Modifier
- * @Date 2019/7/14 16:41
- * @Version 1.0
- **/
+
 @Configuration
 public class ServletListenerRegistrationConfig {
 
